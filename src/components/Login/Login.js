3843290dbe3d6eee2d66517299c3cf036dc5ef88
Barry <blrussell09@gmail.com>
2018-07-13 18:11:13 -0500

    Added Components

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+class Login extends React.Component
+{
+  render ()
+  {
+    return (
+      <h3 className="Login">Login</h3>
+    );
+  };
+};
+
+export default Login;
